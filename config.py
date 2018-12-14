6a05065b7c47c81d131282a6e9a59c75138cb4ed
Ryan Cross <ryancrosscodes@gmail.com>
2018-12-14 07:51:18 -0800

    change to mailgun

--- config.py
+++ config.py
@@ -10,9 +10,9 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     ECHO = True
 
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 465
-    MAIL_USE_SSL = True
+    MAIL_SERVER = 'smtp.mailgun.ord'
+    MAIL_PORT = 587
+    # MAIL_USE_SSL = True
     MAIL_USERNAME = os.getenv("GROCMAILU")
     MAIL_PASSWORD = os.getenv("GROCMAILP")
     MAIL_DEFAULT_SENDER = os.getenv("GROCMAILU")
